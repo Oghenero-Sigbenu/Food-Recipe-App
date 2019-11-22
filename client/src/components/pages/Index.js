4382d5ee6d59c3799f354d49d89f334267c6f01f
Nero-19 <sigbenuoghenero@gmail.com>
2019-11-22 07:02:34 -0800

    code fixes

--- client/src/components/pages/Index.js
+++ client/src/components/pages/Index.js
@@ -1,7 +1,6 @@
 export { default as Home } from "./Home";
 export { default as Recipes} from "./Recipes";
 export { default as CreateRecipe } from "./CreateRecipe";
-export { default as Auth } from "./Auth";
 export { default as Login} from "./Login";
 export { default as Register } from "./Register";
 export {default as RecipeDetail } from "./RecipeDetail";
