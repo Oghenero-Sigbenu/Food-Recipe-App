2c7c0e2c47315449d1a187adbcff07e15934faed
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2020-07-19 03:40:44 +0100

    code fix

--- client/src/utils/axiox.base.js
+++ client/src/utils/axiox.base.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 // import store from "../store";
+require("dotenv").config();
 
 const instance = axios.create({
-	baseURL: "http://localhost:5000/api/v1",
-	// baseURL: process.env.BASE_URL,http://localhost:5000/api/v1
+	// baseURL: "http://localhost:5000/api/v1",
+	baseURL: process.env.BASE_URL,
 	headers: {
 		"Content-Type": "application/json"
 		}
